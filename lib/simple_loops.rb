bf970779d126c8e0861e3a39e818f5842e347480
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-23 21:48:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,7 @@
-def loop_message_five_times(array)
-  counter = 5
-
-  while counter < array.length do
-    puts array[counter]
-    counter += 5
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
   end
 end
